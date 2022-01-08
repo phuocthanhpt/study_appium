7630c4e7bc44a56058a26f95f02acbef4cc5da2e
Thanh Mai <thanh.maiphuoc@doctoranywhere.com>
2022-01-08 23:00:02 +0700

    lab16: practice testNG

---
+++ src/main/java/appium_related/lab16/testng/LoginTestWithTestNG.java
@@ -0,0 +1,35 @@
+package appium_related.lab16.testng;
+
+import appium_related.driver.DriverFactory;
+import appium_related.models.pages.LoginPage;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.Test;
+
+public class LoginTestWithTestNG {
+
+    @Test
+    public void loginWithValidCredentialTest(){
+        DriverFactory.startAppiumServer();
+        AndroidDriver<MobileElement> androidDriver = DriverFactory.getAndroidDriver();
+
+        try {
+            LoginPage loginPage = new LoginPage(androidDriver);
+            loginPage.bottomNavigationComponent().clickOnLoginLabel();
+            loginPage
+                    .inputUsername("teo@sth.com")
+                    .inputPassword("876554321")
+                    .clickOnLoginBtnElem();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            androidDriver.quit();
+        }
+    }
+
+    @Test
+    public void test02(){
+        System.out.println("test 02");
+    }
+}
