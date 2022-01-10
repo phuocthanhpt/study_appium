5246dbee79c8d6561af4b7572cf4457267255833
Thanh Mai <thanh.maiphuoc@doctoranywhere.com>
2022-01-10 20:40:57 +0700

    lab18: use Gson with Data provider

--- src/main/java/appium_related/lab18/LoginTestWithDataProvider.java
+++ src/main/java/appium_related/lab18/LoginTestWithDataProvider.java
@@ -2,31 +2,31 @@ package appium_related.lab18;
 
 import appium_related.driver.DriverFactory;
 import appium_related.lab17.LoginCredential;
-import appium_related.models.BaseTest;
 import appium_related.models.pages.LoginPage;
+import appium_related.utils.data.DataObjectBuilder;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
+public class LoginTestWithDataProvider{
 
-public class LoginTestWithDataProvider extends BaseTest {
-    static{
-        loginCredentialData = Arrays.asList(new LoginCredential("", ""), new LoginCredential("teo@sth.com", "87654321"));
-    }
-
-    @Test
-    public void loginWithInvalidCredential(){
+    @Test(dataProvider = "invalidLoginData")
+    public void loginWithInvalidCredential(LoginCredential loginCredentialData) {
         DriverFactory.startAppiumServer();
         AndroidDriver<MobileElement> androidDriver = DriverFactory.getAndroidDriver();
 
-        for(LoginCredential loginCred : loginCredentialData){
-            LoginPage loginPage = new LoginPage(androidDriver);
-            loginPage.bottomNavigationComponent().clickOnLoginLabel();
-            loginPage
-                    .inputUsername(loginCred.getUsername())
-                    .inputPassword(loginCred.getPassword())
-                    .clickOnLoginBtnElem();
-        }
+        LoginPage loginPage = new LoginPage(androidDriver);
+        loginPage.bottomNavigationComponent().clickOnLoginLabel();
+        loginPage
+                .inputUsername(loginCredentialData.getUsername())
+                .inputPassword(loginCredentialData.getPassword())
+                .clickOnLoginBtnElem();
+    }
+
+    @DataProvider
+    public LoginCredential[] invalidLoginData() {
+        String jsonLocation = "/src/main/java/resources/test-data/LoginInvalidCredential.json";
+        return DataObjectBuilder.buildDataObject(jsonLocation, LoginCredential[].class);
     }
 }
