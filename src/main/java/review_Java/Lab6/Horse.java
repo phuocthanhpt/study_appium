fbfe9d54cda5c5f3bbf2fc8a4ae236792d41907e
phuocthanhpt <phuocthanhpt@gmail.com>
2023-11-01 12:43:39 +0800

    md

---
+++ src/main/java/review_Java/Lab6/Horse.java
@@ -0,0 +1,9 @@
+package review_Java.Lab6;
+
+public class Horse extends RacingAnimal {
+
+    public Horse(int maxSpeed, String name){
+        this.
+    }
+
+}
--- src/main/java/review_Java/Lab6/RacingAnimal.java
+++ src/main/java/review_Java/Lab6/RacingAnimal.java
@@ -3,18 +3,34 @@ package review_Java.Lab6;
 import java.security.SecureRandom;
 
 public class RacingAnimal {
-    int maxRacingSpeed;
-    String name;
+    private int maxRacingSpeed;
+    private String name;
 
-    public RacingAnimal(int maxRacingSpeed, String name) {
+    public int getMaxRacingSpeed() {
+        return maxRacingSpeed;
+    }
+
+    public void setMaxRacingSpeed(int maxRacingSpeed) {
         this.maxRacingSpeed = maxRacingSpeed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public int speed() {
-        int randomSpeed = new SecureRandom().nextInt(this.maxRacingSpeed);
-        System.out.printf("%s is created with speed <%d>%n", this.name, randomSpeed);
-        return randomSpeed;
-    }
+    //    public RacingAnimal(int maxRacingSpeed, String name) {
+//        this.maxRacingSpeed = maxRacingSpeed;
+//        this.name = name;
+//    }
+//
+//    public int speed() {
+//        int randomSpeed = new SecureRandom().nextInt(this.maxRacingSpeed);
+//        System.out.printf("%s is created with speed <%d>%n", this.name, randomSpeed);
+//        return randomSpeed;
+//    }
 
 }
